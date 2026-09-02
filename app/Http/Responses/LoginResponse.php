8b9f6056777c3ef95794c6ebd6e2125ab3369b21
patrickturc <patrique_turchette@hotmail.com>
2026-09-01 21:10:26 -0300

    feat: implement custom login response to redirect admin users to the dev dashboard

---
+++ app/Http/Responses/LoginResponse.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
+use Illuminate\Support\Facades\Auth;
+
+class LoginResponse implements LoginResponseContract
+{
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function toResponse($request)
+    {
+        $user = Auth::user();
+
+        if ($user && $user->is_dev_admin) {
+            return redirect()->intended(route('dev-admin.dashboard'));
+        }
+
+        return redirect()->intended(route('dashboard'));
+    }
+}
